db7231f6ecbb5c5ac0c76cc9396a78c5994d57f0
Oleg Matrokhin <oyumatrokhin@avito.ru>
2017-08-03 14:35:02 +0300

    return empty result if metric not found

--- expr/expr.go
+++ expr/expr.go
@@ -3971,10 +3971,11 @@ type seriesFunc func(*MetricData, *MetricData) *MetricData
 
 func forEachSeriesDo(e *expr, from, until int32, values map[MetricRequest][]*MetricData, function seriesFunc) ([]*MetricData, error) {
 	arg, err := getSeriesArg(e.args[0], from, until, values)
-	if err != nil {
-		return nil, ErrMissingTimeseries
-	}
 	var results []*MetricData
+	if err != nil {
+		return results, nil
+		// return nil, ErrMissingTimeseries
+	}
 
 	for _, a := range arg {
 		r := *a
